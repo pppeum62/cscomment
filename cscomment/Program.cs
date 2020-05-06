82210146b0983e9db68a0b33fb3a5674240c99c2
Gi Ppeum, Lee <48782921+pppeum62@users.noreply.github.com>
2020-05-06 18:53:13 +0900

    0506 03번 실습 문제

--- cscomment/Program.cs
+++ cscomment/Program.cs
@@ -10,6 +10,22 @@ namespace cscomment
     {
         static void Main(string[] args)
         {
+            int x, y;
+            Console.Write("x = ");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("y = ");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > 4)
+            {
+                if (y > 2)
+                {
+                    Console.WriteLine(x * y);
+                }
+            } else
+            {
+                Console.WriteLine();
+            }
         }
     }
 }
